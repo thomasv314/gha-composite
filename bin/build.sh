00f328b666d86d864ccc5cc90273a766d7ce78af
Thomas Vendetta <thomas.vendetta@leaflink.com>
2022-12-07 16:59:38 -0500

    fix: updates

--- bin/build.sh
+++ bin/build.sh
@@ -1,2 +1,2 @@
-$(npm bin)/ncc build src/actions/wait/index.js -o dist/wait --source-map
-$(npm bin)/ncc build src/actions/parse-arguments/index.js -o dist/parse-arguments --source-map
+$(npm bin)/ncc build src/actions/wait/index.js -o actions/wait/dist --source-map
+$(npm bin)/ncc build src/actions/parse-arguments/index.js -o actions/parse-arguments/dist --source-map
