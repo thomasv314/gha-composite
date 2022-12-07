cb9e9fa3f1376bceab1e533c5f737e24b9ab96a9
Thomas Vendetta <thomas.vendetta@leaflink.com>
2022-12-07 17:50:05 -0500

    intial commit

--- actions/parse-arguments/index.js
+++ actions/parse-arguments/index.js
@@ -1,18 +1,27 @@
+const github = require('@actions/github');
 const core = require('@actions/core');
-const wait = require('./wait');
 
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const token = core.getInput('github-token');
+    const octokit = github.getOctokit(token)
+    // You can also pass in additional options as a second parameter to getOctokit
+    // const octokit = github.getOctokit(myToken, {userAgent: "MyActionVersion1"});
+    console.log(JSON.stringify(github.context, null, 4))
+    const { data: pullRequest } = await octokit.rest.pulls.get({
+        owner: 'octokit',
+        repo: 'rest.js',
+        pull_number: 123,
+        mediaType: {
+          format: 'diff'
+        }
+    });
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    console.log(pullRequest);
 
-    core.setOutput('time', new Date().toTimeString());
+
   } catch (error) {
     core.setFailed(error.message);
   }
