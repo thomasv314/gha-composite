266a822322c8051382c26b329e4cff81ebeab362
Thomas Vendetta <thomas.vendetta@leaflink.com>
2022-12-07 18:11:40 -0500

    update

--- actions/parse-arguments/index.js
+++ actions/parse-arguments/index.js
@@ -9,16 +9,19 @@ async function run() {
     const octokit = github.getOctokit(token)
     // You can also pass in additional options as a second parameter to getOctokit
     // const octokit = github.getOctokit(myToken, {userAgent: "MyActionVersion1"});
-    console.log(JSON.stringify(github.context, null, 4))
 
-    owner = github.context.repository.owner.login
+    console.log(JSON.stringify(github.context.payload, null, 4))
+
+    console.log("CONTEXT", github.context.repo)
+
+    owner = "leaflink"
     repo = github.context.repository.name
     pull_number = github.context.issue.number
 
     console.log("owner", owner, "repo", repo, "pull_number", pull_number)
     const data = await octokit.rest.pulls.listCommentsForReview({
-      owner,
-      repo,
+      owner: github,
+      repo: github.context.repo,
       pull_number,
     });
 
