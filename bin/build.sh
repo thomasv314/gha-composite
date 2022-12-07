e378db5847a8fc08f6296c0eed1fe5744b5bc241
Thomas Vendetta <thomas.vendetta@leaflink.com>
2022-12-07 17:41:11 -0500

    restart

--- bin/build.sh
+++
@@ -1,2 +0,0 @@
-$(npm bin)/ncc build src/actions/wait/index.js -o actions/wait/dist --source-map
-$(npm bin)/ncc build src/actions/parse-arguments/index.js -o actions/parse-arguments/dist --source-map
